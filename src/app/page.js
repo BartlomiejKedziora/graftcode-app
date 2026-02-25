5d0ab1095264b5c7df36533b985988314235c418
Bartek Kedziora <kedzitheeagle@Kedzis-Mac-mini.local>
2026-02-25 21:00:40 +0100

    Graftcode configuration

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,26 @@
 "use client";
 
+import * as Graft from "@graft/npm-backend";
+
 export default function Home() {
   const handleClick = async () => {
-    const res = await fetch("/api/hello");
-    const data = await res.json();
-    console.log(data);
+    try {
+      Graft.GraftConfig.host = "ws://localhost:8080/ws";
+
+      const backend = new Graft.Backend();
+      const result = await backend.sayHello();
+      alert(result);
+    } catch (error) {
+      console.error("ERROR:", error);
+    }
   };
 
   return (
     <div style={{ padding: 40 }}>
-      <h1>Graft Test</h1>
-      <button onClick={handleClick}>Test backend</button>
+      <h1>Graftcode Gateway</h1>
+      <button style={{ marginTop: 20 }} onClick={handleClick}>
+        Cześć!
+      </button>
     </div>
   );
 }
